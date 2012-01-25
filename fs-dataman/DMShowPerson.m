041f0e4dadffc2ac50ee603776125cb42440b38c
Chris Miller <nserror@me.com>
2012-01-25 12:52:00 -0700

    Show person works.

--- fs-dataman/DMShowPerson.m
+++ fs-dataman/DMShowPerson.m
@@ -8,19 +8,66 @@
 
 #import "DMShowPerson.h"
 
-@implementation DMShowPerson
+#import "Console.h"
+
+#import "NDService.h"
+#import "NDService+FamilyTree.h"
+#import "NSData+StringValue.h"
+
+@implementation DMShowPerson {
+    NSString* _personId;
+}
 
 + (void)load
 {
-// UNCOMMENT WHEN THE COMMAND IS COMPLETE
-//    @autoreleasepool {
-//        [[DMVerb registeredCommands] addObject:[self class]];
-//    }
+    @autoreleasepool {
+        [[DMVerb registeredCommands] addObject:[self class]];
+    }
 }
 
 + (NSString*)verbCommand
 {
     return @"show-person";
+}
+
+- (NSString*)description
+{
+    return [NSString stringWithFormat:@"SHOW-PERSON %@", _personId?:@"self"];
+}
+
+- (void)processArgs
+{
+    if (1<[self.arguments count]) {
+        dm_PrintLnThenDie(@"Improper number of arguments. I only understand how to fetch the information for one record at a time");
+    }
+    
+    NSArray* me_synonyms = [NSArray arrayWithObjects:@"me", @"myself", @"self", nil];
+    
+    if (1==[self.arguments count]) {
+        _personId = [self.arguments objectAtIndex:0];
+        if ([me_synonyms containsObject:_personId]) _personId = nil;
+        // otherwise treat it like a person id
+    } else {
+        _personId=nil;
+    }
+}
+
+- (void)run
+{
+    NSMutableDictionary* params = [NDFamilyTreeAllPersonReadValues() mutableCopy];
+    [params removeObjectForKey:NDFamilyTreeReadRequestParameter.personas];
+    [self.service familyTreeReadPersons:nil==_personId?nil:[NSArray arrayWithObject:_personId]
+                         withParameters:params
+                              onSuccess:^(NSHTTPURLResponse *resp, id response, NSData *payload) {
+                                  NSError* err=nil;
+                                  NSData* json = [NSJSONSerialization dataWithJSONObject:response options:NSJSONWritingPrettyPrinted error:&err];
+                                  if (err) dm_PrintLn(@"Can't re-emit JSON. Dude, that's freakin weird.");
+                                  else dm_PrintLn(@"%@", [json fs_stringValue]);
+                              } 
+                              onFailure:^(NSHTTPURLResponse *resp, NSData *payload, NSError *error) {
+                                  dm_PrintURLOperationResponse(resp, payload, error);
+                              }];
+    [self.service.operationQueue waitUntilAllOperationsAreFinished];
 }
 
 @end
