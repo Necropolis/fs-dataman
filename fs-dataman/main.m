770d2e4f0dbf1f77fbd03ea8817677840121a7f2
Chris Miller <nserror@me.com>
2012-02-20 15:31:56 -0700

    New toys.

--- fs-dataman/main.m
+++ fs-dataman/main.m
@@ -11,11 +11,38 @@
 #import "Console.h"
 
 #import "DMVerb.h"
+#if defined (DEBUG) && defined (FSURLDEBUG)
+#import "FSURLOperation.h"
+#endif
 
 int main (int argc, const char * argv[])
 {
 
     @autoreleasepool {
+        
+#if defined (DEBUG) && defined (FSURLDEBUG)
+        __block size_t concurrentOperations = 0;
+        __block NSError * ___error=nil;
+        __block NSRegularExpression * devKeyRegex = [[NSRegularExpression alloc] initWithPattern:@"&?key\\=((?:\\w{4}\\-?){8})" options:0 error:&___error];
+        [[FSURLOperation debugCallbacks] addObject:^(NSURLRequest * request, enum FSURLDebugStatus status, NSHTTPURLResponse * response, NSData * payload, NSError * error) {
+            NSString * requestURL = [request.URL description];
+            requestURL = [devKeyRegex stringByReplacingMatchesInString:requestURL options:0 range:NSMakeRange(0, [requestURL length]) withTemplate:@"&key=****-****-****-****-****-****-****-****"];
+            switch (status) {
+                case RequestBegan:
+                    ++concurrentOperations;
+                    dm_PrintLn(@"Concurrent Operations: %3lu; Started %@ %@", concurrentOperations, request.HTTPMethod, requestURL);
+                    break;
+                case RequestFinished:
+                    --concurrentOperations;
+                    dm_PrintLn(@"Concurrent Operations: %3lu; Finish  %@ %@", concurrentOperations, request.HTTPMethod, requestURL);
+                    break;
+                    
+                default:
+                    dm_PrintLnThenDie(@"dude, this isn't a known callback type");
+                    break;
+            }
+        }];
+#endif
         
         NSArray* args = [[NSProcessInfo processInfo] arguments];
         
